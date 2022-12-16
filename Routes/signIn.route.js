2bf02176c9420857051e3b44e9d7097dd6944dbf
masum <mr.razamasum@gmail.com>
2022-12-16 17:39:17 +0530

    added

--- Routes/signIn.route.js
+++ Routes/signIn.route.js
@@ -12,12 +12,13 @@ signIn.post("/",async(req,res)=>{
     const {email,password}=req.body;
     try{
         const user=await SignUpModel.find({email})
+
         if(user.length>0){
             const hash=user[0].password
             bcrypt.compare(password,hash,(err,result)=>{
                 if(result){
                     const token=jwt.sign({"UserID":user[0]._id},process.env.private_key)
-                    res.status(200).send({message:"Login Successful",token,name:user.name})
+                    res.status(200).send({message:"Login Successful",token,user})
                 }else{
                     res.status(400).send({message:"Please logIn Again"})
                 }
